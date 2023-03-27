791259be26de505f08ec4fd5b8a696e0c9b9481b
Jhonatan Duarte <jhonatanvitalis123@hotmail.com>
2023-03-27 19:02:19 -0300

    Setup inicial

---
+++ src/main/java/br/senac/constructor/ConstructorApplication.java
@@ -0,0 +1,13 @@
+package br.senac.constructor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/constructor/ConstructorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.constructor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
